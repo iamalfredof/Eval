3904a80afbb953c13615c7bd6bb9411822d98542
Patricio Jeri <pjeri@hotmail.com>
2016-03-01 10:50:37 -0500

    Deploy recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,6 @@ namespace :sidekiq do
   end
   task :restart do
     on roles(:app) do
-    	execute :sudo, :initctl, :stop, :workers
       execute :sudo, :initctl, :start, :workers
     end
   end
