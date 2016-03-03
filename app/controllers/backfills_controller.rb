6057ce24acd5ede823f83c0aae4eb09705a0dde0
Patricio Jeri <pjeri@hotmail.com>
2016-03-03 10:53:31 -0500

    Backfill worker

--- app/controllers/backfills_controller.rb
+++ app/controllers/backfills_controller.rb
@@ -1,13 +1,7 @@
 class BackfillsController < ApplicationController
 
 	def index
-		documents = Document.all
-	
-		documents.each do |d|
-			random_hex = d.html_url.split('/')[4].split('-').last
-			BackfillWorker.perform_async(d.foreign_document_url, d.foreign_document_id, random_hex)
-			sleep 1
-		end
+		BackfillWorker.perform_async
 	end
 
 end
--- app/workers/backfill_worker.rb
+++ app/workers/backfill_worker.rb
@@ -2,9 +2,15 @@ class BackfillWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
 
-  def perform(foreign_document_url, foreign_document_id, random_hex)
-		dp = DocumentProcessor.new(foreign_document_url, foreign_document_id, random_hex)
-		dp.process_plain_text_backfill!
+  def perform
+  	documents = Document.all
+	
+		documents.each do |d|
+			random_hex = d.html_url.split('/')[4].split('-').last
+			dp = DocumentProcessor.new(d.foreign_document_url, d.foreign_document_id, random_hex)
+			dp.process_plain_text_backfill!
+			Rails.logger.info "---- Backfilled: " + d.foreign_document_id.to_s + " ----"
+		end
   end
   
 end
