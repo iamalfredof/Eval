87f21d4869b10c870f6c5780b5f402db4065956b
Patricio Jeri <pjeri@hotmail.com>
2016-06-28 16:05:47 -0400

    P

--- app/helpers/document_word_search.rb
+++ app/helpers/document_word_search.rb
@@ -33,12 +33,12 @@ class DocumentWordSearch
 		# Read file and build data
 		lines = IO.readlines( cache_route + '/' + filename )
 		for line in lines
+			if line["<page-break>"]
+	        current_page += line.scan(/<page-break>/).count
+	    end
 			if line.downcase.include? query.downcase
 				data << { "p" => current_page, "t" => line  }
 			end
-	    if line["<page-break>"]
-	        current_page += line.scan(/<page-break>/).count
-	    end
 		end
 
 		return data
