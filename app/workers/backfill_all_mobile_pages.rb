f1edd9b4131e7cf7d55a61f1f04c87c6bdf36c95
Patricio Jeri <pjeri@hotmail.com>
2016-08-18 16:00:40 -0400

    FIFO backfill

--- app/workers/backfill_all_mobile_pages.rb
+++ app/workers/backfill_all_mobile_pages.rb
@@ -6,7 +6,7 @@ class BackfillAllMobilePagesWorker
   	next_doc = Document.where(:backfilled => false).first
 
   	ProcessMobilePagesWorker.perform_in(
-					2.seconds
+					2.seconds,
 					next_doc.foreign_document_url,
 					next_doc.foreign_document_id,
 					next_doc.html_url.split('/')[4].split('-')[1]
