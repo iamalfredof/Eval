e40c49e9fb107a11dcd91f1ad8fc9da6a70e2864
Patricio Jeri <pjeri@hotmail.com>
2017-01-05 17:10:33 -0500

    Added Ricardo to push notifications

--- app/workers/pushover_worker.rb
+++ app/workers/pushover_worker.rb
@@ -4,12 +4,20 @@ class PushoverWorker
   sidekiq_options :queue => :default
 
   def perform(title, message)
-    # Carlos' credentials are being used
+    # Carlos' credentials
     Pushover.notification(
     	title: title,
       message: message,
       user: 'u4ignr2qcqsxray22nxjj792qfgras',
       token: 'a34roc2qfzdtzkm242g13wh5tc7ua9'
+    )
+
+    # Ricardo's credentials
+    Pushover.notification(
+      title: title,
+      message: message,
+      user: 'uj9g7pgk8njoam6866fwhn9c2e21vz',
+      token: 'ayvak424nb9pqhsvvosxf5wswtht5y'
     )
   end
   
