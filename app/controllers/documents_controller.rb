943b1e79c11f3a1114dc5422db71e25a138b2595
ricardo <ricardo.izasiga@gmail.com>
2017-03-07 11:40:52 -0500

    respond_as_json

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -19,38 +19,34 @@ class DocumentsController < ApplicationController
 	end
 
 	def async_process
-		respond_to do |format|
-      if @document.save
-      	random_hex = SecureRandom.hex
+    if @document.save
+    	random_hex = SecureRandom.hex
 
-      	AsyncPDFWorker.perform_async(@document.foreign_document_url, @document.foreign_document_id, 
-      	random_hex, params[:bucket], params[:callback_url])
-      	format.json { render :show, status: :created, location: @document }
-      else
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
+    	AsyncPDFWorker.perform_async(@document.foreign_document_url, @document.foreign_document_id, 
+    	random_hex, params[:bucket], params[:callback_url])
+    	render json: @document, status: :created, location: @document
+    else
+      render json:  @document.errors, status: :unprocessable_entity
     end
 	end
 
 	# POST /documents.json
 	def create
-		respond_to do |format|
-      if @document.save
-      	random_hex = SecureRandom.hex
-				# Process as office or as pdf
-      	if OfficeProcessor.new( @document.foreign_document_url ).is_office?
-      		OfficeWorker.perform_async(@document.foreign_document_url, @document.foreign_document_id, random_hex)
-      	else
-      		PDFWorker.perform_async(@document.foreign_document_url, @document.foreign_document_id, random_hex)
-      	end
+    if @document.save
+    	random_hex = SecureRandom.hex
+			# Process as office or as pdf
+    	if OfficeProcessor.new( @document.foreign_document_url ).is_office?
+    		OfficeWorker.perform_async(@document.foreign_document_url, @document.foreign_document_id, random_hex)
+    	else
+    		PDFWorker.perform_async(@document.foreign_document_url, @document.foreign_document_id, random_hex)
+    	end
 
-      	dp = DocumentProcessor.new(@document.foreign_document_url, @document.foreign_document_id, random_hex)
-      	html_url = dp.get_html_url
-    		@document.update_attribute(:html_url, html_url)
-      	format.json { render :show, status: :created, location: @document }
-      else
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
+    	dp = DocumentProcessor.new(@document.foreign_document_url, @document.foreign_document_id, random_hex)
+    	html_url = dp.get_html_url
+  		@document.update_attribute(:html_url, html_url)
+    	render json: @document, status: :created, location: @document
+    else
+      render json:  @document.errors, status: :unprocessable_entity
     end
 	end
 
